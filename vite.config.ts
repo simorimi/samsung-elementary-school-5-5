004d7ed67fbc38641165835257ba79c8f0130fea
simhorim <hori5020@gmail.com>
2025-10-31 04:52:16 +0900

    chore: config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/samsung-elementary-school-5-5"
+      : "/",
 });
